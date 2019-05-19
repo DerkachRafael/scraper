06f6dd3dbe477152a5f342278bfcd30754c66292
rafael <derkach.rafael@gmail.com>
2019-05-19 23:45:09 +0300

    [scrapWithPuppeteer]

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-import {app} from "./server";
-import './cron';
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+
+const JSONToFile = (obj, filename) => fs.writeFile(`${filename}.json`, JSON.stringify(obj, null, 2), (e) => console.log(e));
+
+const scrape = async () => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto('http://hdrezka-ag.com/?filter=popular', { waitUntil: 'domcontentloaded' });
+
+    const result = await page.evaluate( () => {
+        const getFirstNthItems =  (arr, num) => arr.slice(0, num);
+        const parentSelector = '.b-content__inline_item';
+        const domElements = Array.from(document.querySelectorAll(parentSelector));
+
+        return getFirstNthItems(domElements, 10).map( elem => ({
+                title: elem.querySelector('.b-content__inline_item-link').innerText,
+                link: elem.querySelector('.b-content__inline_item-link a').href
+        }));
+
+    });
+
+    await browser.close();
+    return result;
 
 
- function init () {
-    app.listen(9002);
-}
+};
 
-init();
+scrape()
+    .then(data => JSONToFile(data, 'top10fromHdRezka'))
+    .catch( e => console.log(e));
---
+++ readme.md
@@ -0,0 +1,5 @@
+_scrap with **puppeteer** top 10 films/serials hdrezka website_
+****
+  Run in the terminal
+        
+    npm run dev
